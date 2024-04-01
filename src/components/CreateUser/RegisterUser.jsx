a6a84c5315b43181fe31764bfc5a2c60e15f67e8
Bishnu <jasbis2001@gmail.com>
2024-04-01 10:05:14 +0530

    create register component

--- src/components/CreateUser/RegisterUser.jsx
+++ src/components/CreateUser/RegisterUser.jsx
@@ -34,7 +34,7 @@ const RegisterUser = () => {
             className="block text-gray-700 text-sm font-semibold mb-1"
             htmlFor="userId"
           >
-            User ID
+            Empolyee ID
           </label>
           <input
             className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
